5edb4526223166dd64042dca0bcc4b8753992823
macrocosme <vohl@astron.nl>
2019-12-20 12:13:50 +0100

    Add gedits

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -16,38 +16,46 @@ $(document).ready(function() {
 			}
 		}
 	});
-});
 
-// $(document).ready(function() {
-// 	$('.popup-with-zoom-anim').magnificPopup({
-// 		type: 'inline',
-//
-// 		fixedContentPos: false,
-// 		fixedBgPos: true,
-//
-// 		overflowY: 'auto',
-//
-// 		closeBtnInside: true,
-// 		preloader: false,
-//
-// 		midClick: true,
-// 		removalDelay: 300,
-// 		mainClass: 'my-mfp-zoom-in'
-// 	});
-//
-// 	$('.popup-with-move-anim').magnificPopup({
-// 		type: 'inline',
-//
-// 		fixedContentPos: false,
-// 		fixedBgPos: true,
-//
-// 		overflowY: 'auto',
-//
-// 		closeBtnInside: true,
-// 		preloader: false,
-//
-// 		midClick: true,
-// 		removalDelay: 300,
-// 		mainClass: 'my-mfp-slide-bottom'
-// 	});
-// });
+	$('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false
+	});
+
+	$('.popup-with-zoom-anim').magnificPopup({
+			type: 'inline',
+
+			fixedContentPos: false,
+			fixedBgPos: true,
+
+			overflowY: 'auto',
+
+			closeBtnInside: true,
+			preloader: false,
+
+			midClick: true,
+			removalDelay: 300,
+			mainClass: 'my-mfp-zoom-in'
+		});
+
+		$('.popup-with-move-anim').magnificPopup({
+			type: 'inline',
+
+			fixedContentPos: false,
+			fixedBgPos: true,
+
+			overflowY: 'auto',
+
+			closeBtnInside: true,
+			preloader: false,
+
+			midClick: true,
+			removalDelay: 300,
+			mainClass: 'my-mfp-slide-bottom'
+		});
+});
