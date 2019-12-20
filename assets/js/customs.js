4c2eee5ea1a81e2b456271ff0929dc588f8e9c7a
macrocosme <vohl@astron.nl>
2019-12-20 15:29:56 +0100

    Add vids

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -24,7 +24,95 @@ $(document).ready(function() {
 		removalDelay: 160,
 		preloader: false,
 
-		fixedContentPos: false
+		fixedContentPos: false,
+
+		iframe: {
+		  markup: '<div class="mfp-iframe-scaler">'+
+		            '<div class="mfp-close"></div>'+
+		            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+		          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+
+		  patterns: {
+		    youtube: {
+		      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+		      id: 'v=', // String that splits URL in a two parts, second part should be %id%
+		      // Or null - full URL will be returned
+		      // Or a function that should return %id%, for example:
+		      // id: function(url) { return 'parsed id'; }
+
+		      src: '//www.youtube.com/embed/%id%?autoplay=1' // URL that will be set as a source for iframe.
+		    },
+		    vimeo: {
+		      index: 'vimeo.com/',
+		      id: '/',
+		      src: '//player.vimeo.com/video/%id%?autoplay=1'
+		    },
+		    gmaps: {
+		      index: '//maps.google.',
+		      src: '%id%&output=embed'
+		    }
+
+		    // you may add here more sources
+
+		  },
+
+		  srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+		}
+	});
+
+	$('.popup-youtube-list').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false,
+
+		iframe: {
+		  markup: '<div class="mfp-iframe-scaler">'+
+		            '<div class="mfp-close"></div>'+
+		            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+		          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+
+		  patterns: {
+		    youtube: {
+		      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+					lst: function(url) {
+							  if(!url) url = location.search;
+							  var query = url.substr(1);
+							  var result = {};
+							  query.split("&").forEach(function(part) {
+							    var item = part.split("=");
+							    result[item[0]] = decodeURIComponent(item[1]);
+							  });
+							  return result[1];
+							},
+		      id: function(url) {
+							  if(!url) url = location.search;
+							  var query = url.substr(1);
+							  var result = {};
+							  query.split("&").forEach(function(part) {
+							    var item = part.split("=");
+							    result[item[0]] = decodeURIComponent(item[1]);
+							  });
+							  return result[0];
+							}, // String that splits URL in a two parts, second part should be %id%
+		      // Or null - full URL will be returned
+		      // Or a function that should return %id%, for example:
+		      // id: function(url) { return 'parsed id'; }
+
+		      src: '//www.youtube.com/embed/%id%&%lst%?autoplay=1' // URL that will be set as a source for iframe.
+		    }
+
+		    // you may add here more sources
+
+		  },
+
+		  srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+		}
 	});
 
 	$('.popup-with-zoom-anim').magnificPopup({
