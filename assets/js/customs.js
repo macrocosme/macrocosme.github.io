f89a49a97ce98e624db7ebd1c184565ab902721d
macrocosme <vohl@astron.nl>
2019-12-20 15:36:42 +0100

    Add vids

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -80,31 +80,12 @@ $(document).ready(function() {
 		    youtube: {
 		      index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
 
-					lst: function(url) {
-							  if(!url) url = location.search;
-							  var query = url.substr(1);
-							  var result = {};
-							  query.split("&").forEach(function(part) {
-							    var item = part.split("=");
-							    result[item[0]] = decodeURIComponent(item[1]);
-							  });
-							  return result[1];
-							},
-		      id: function(url) {
-							  if(!url) url = location.search;
-							  var query = url.substr(1);
-							  var result = {};
-							  query.split("&").forEach(function(part) {
-							    var item = part.split("=");
-							    result[item[0]] = decodeURIComponent(item[1]);
-							  });
-							  return result[0];
-							}, // String that splits URL in a two parts, second part should be %id%
+					id: 'list=', // String that splits URL in a two parts, second part should be %id%
 		      // Or null - full URL will be returned
 		      // Or a function that should return %id%, for example:
 		      // id: function(url) { return 'parsed id'; }
 
-		      src: '//www.youtube.com/embed/%id%&%lst%?autoplay=1' // URL that will be set as a source for iframe.
+		      src: '//www.youtube.com/embed/videoseries?%id%?autoplay=1' // URL that will be set as a source for iframe.
 		    }
 
 		    // you may add here more sources
