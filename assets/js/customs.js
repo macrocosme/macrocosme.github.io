61f9f7d89bd68f89f2a8fda1e9190ae7b8eb237d
macrocosme <vohl@astron.nl>
2019-12-22 08:15:46 +0100

    Add workshop paper

--- assets/js/customs.js
+++ assets/js/customs.js
@@ -61,6 +61,38 @@ $(document).ready(function() {
 		}
 	});
 
+	$('.popup-webpage').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+
+		fixedContentPos: false,
+
+		iframe: {
+		  markup: '<div class="mfp-iframe-scaler">'+
+		            '<div class="mfp-close"></div>'+
+		            '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+		          '</div>', // HTML markup of popup, `mfp-close` will be replaced by the close button
+
+		  patterns: {
+		    youtube: {
+		      index: item.el.attr('website'), // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+
+		      id: 'v=', // String that splits URL in a two parts, second part should be %id%
+		      // Or null - full URL will be returned
+		      // Or a function that should return %id%, for example:
+		      // id: function(url) { return 'parsed id'; }
+
+		      src: item.el.attr('website') // URL that will be set as a source for iframe.
+		    }
+		  },
+
+		  srcAction: 'iframe_src', // Templating object key. First part defines CSS selector, second attribute. "iframe_src" means: find "iframe" and set attribute "src".
+		}
+	});
+
 	$('.popup-youtube-list').magnificPopup({
 		disableOn: 700,
 		type: 'iframe',
